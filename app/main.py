7bffc0cf45a52bc3efdaa7800b4c06fbd9284a6b
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-09-26 13:27:05 +0300

    feat: remove extra endpoints

--- app/main.py
+++ app/main.py
@@ -7,11 +7,3 @@ app = FastAPI(
 )
 
 app.include_router(main_router)
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
